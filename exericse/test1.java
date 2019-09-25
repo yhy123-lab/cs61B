2fe99859e23b4e924d97a9c28d5984fabebba7f5
yuhaoyang123 <39150374+yuhaoyang123@users.noreply.github.com>
2019-09-25 17:15:50 +0800

    Update test1.java

--- exericse/test1.java
+++ exericse/test1.java
@@ -95,3 +95,35 @@ class Triangle{
         System.out.println(",t5 area = " + t5.area);
     }
 }
+class GoodDog {
+    private int size;
+    public int getsize(){
+        return size;
+    }
+    public void setSize(int s){
+        size = s;
+    }
+    void bark(){
+        if (size > 60){
+            System.out.println("Wooof! Wooof!");
+        }
+        else if (size > 14){
+            System.out.println("Ruff! Ruff!");
+        }else{
+            System.out.println("Yip! Yip!");
+        }
+    }
+}
+
+class GoodDogTestDrive{
+    public static void main (String[] args){
+        GoodDog one = new GoodDog();
+        one.setSize(70);
+        GoodDog two = new GoodDog();
+        two.setSize(8);
+        System.out.println("Dog one:" + one.getsize());
+        System.out.println("Dog two:" + two.getsize());
+        one.bark();
+        two.bark();
+    }
+}
